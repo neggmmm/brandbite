9b2b6e66ab6ff51920dc85ef869acec4e6b61845
neggmmm <kekkdead@gmail.com>
2025-11-17 18:01:25 +0200

    add review

---
+++ server/src/middlewares/uploadCloudinary.middleware.js
@@ -0,0 +1,15 @@
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+import cloudinary from "../config/cloudinary.js";
+
+// Configure storage
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "reviews",                // folder in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png"],
+  },
+});
+
+// Multer middleware
+export const uploadCloud  = multer({ storage });
