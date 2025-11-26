faa19ee6aa7f31f1dfdaecc054c1dcfae4c4ae41
Hager Eissa <hagersalah050@gmail.com>
2025-11-26 03:19:19 +0200

    add localization

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { Home, Utensils, Clock4 , Star, Gift } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 export default function Navbar() {
+  const { t, i18n } = useTranslation();
+  
   const location = useLocation();
 
   const isActive = (path) =>
@@ -12,30 +15,31 @@ export default function Navbar() {
   return (
     <div className="w-full bg-white  shadow-sm fixed bottom-0 left-0 ">
       <div className="w-full max-w-xl mx-auto h-14 flex justify-around items-center px-4">
-
+        {i18n.language=="en"&&<button onClick={() => i18n.changeLanguage("ar")}>AR</button>}
+        {i18n.language=="ar"&&<button onClick={() => i18n.changeLanguage("en")}>EN</button>}
         <Link to="/" className={`flex flex-col items-center ${isActive("/")}`}>
           <Home size={20} />
-          <span className="text-xs">Home</span>
+          <span className="text-xs">{t("home")}</span>
         </Link>
 
         <Link to="/menu" className={`flex flex-col items-center ${isActive("/menu")}`}>
           <Utensils size={20} />
-          <span className="text-xs">Menu</span>
+          <span className="text-xs">{t("menu")}</span>
         </Link>
 
         <Link to="/orders" className={`flex flex-col items-center ${isActive("/orders")}`}>
           <Clock4  size={20} />
-          <span className="text-xs">Orders</span>
+          <span className="text-xs">{t("orders")}</span>
         </Link>
 
         <Link to="/reviews" className={`flex flex-col items-center ${isActive("/reviews")}`}>
           <Star size={20} />
-          <span className="text-xs">Reviews</span>
+          <span className="text-xs">{t("reviews")}</span>
         </Link>
 
         <Link to="/rewards" className={`flex flex-col items-center ${isActive("/rewards")}`}>
           <Gift size={20} />
-          <span className="text-xs">Rewards</span>
+          <span className="text-xs">{t("rewards")}</span>
         </Link>
 
       </div>
