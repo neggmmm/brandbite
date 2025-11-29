1d1b700a3003a7ad084a272d4ce0e3aec6a5df0e
Hager Eissa <hagersalah050@gmail.com>
2025-11-30 01:32:22 +0200

    add cart init,add product slice for redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,29 @@ import RewardPage from "./pages/RewardPage";
 import Admin from "./pages/admin/Admin";
 import AppLayout from "./layout/admin-layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
+import CartPage from "./pages/CartPage";
+import store from "./redux/store/store";
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/reviews" element={<ReviewsPage />} />
-          <Route path="/rewards" element={<RewardPage />} />
-          {/* Single Admin Page with section sub-route */}
-          <Route element={<AppLayout />}>
-            <Route path="/admin/:section?" element={<Admin />} />
-          </Route>
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/reviews" element={<ReviewsPage />} />
+            <Route path="/rewards" element={<RewardPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            {/* Single Admin Page with section sub-route */}
+            <Route element={<AppLayout />}>
+              <Route path="/admin/:section?" element={<Admin />} />
+            </Route>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ client/src/redux/store/slices/ProductSlice.js
@@ -0,0 +1,179 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API = "http://localhost:3000/api/products";
+
+// Fetch All Products
+export const fetchProducts = createAsyncThunk(
+    "products/fetchAll",
+    async () => {
+        const res = await axios.get(`${API}`);
+        return res.data;
+    }
+);
+
+//Fetch product by id
+export const fetchProductById = createAsyncThunk(
+    "products/fetchById",
+    async (id) => {
+        const res = await axios.get(`${API}/${id}`);
+        return res.data;
+    }
+);
+
+//Fetch new product 
+export const fetchNewProducts  = createAsyncThunk(
+    "products/getNew",
+    async () => {
+        const res = await axios.get(`${API}/new`);
+        return res.data;
+    }
+);
+
+//Fetch product (filtered, sorted, paginated)
+export const fetchProductList = createAsyncThunk(
+    "products/getList",
+    async (params) => {
+        // params ممكن تكون { searchTerm, categoryId, sortBy, sortOrder, page, pageSize }
+        const res = await axios.get(`${API}/list`, { params });
+        return res.data;
+    }
+);
+
+//create product
+export const createProduct = createAsyncThunk(
+    "products/create",
+    async (productData) => {
+        const res = await axios.post(`${API}`, productData,{
+                withCredentials: true,      
+                headers: { "Content-Type": "multipart/form-data" }
+            });
+        return res.data;
+    }
+)
+
+//update product
+export const updateProduct = createAsyncThunk(
+    "products/update",
+    async ({ id, productData }) => {
+        const res = await axios.put(`${API}/${id}`, productData,{
+                withCredentials: true,      
+                headers: { "Content-Type": "multipart/form-data" }
+            });
+        return res.data;
+    }
+)
+
+//delete product
+export const deleteProduct = createAsyncThunk(
+    "products/delete",
+    async (id) => {
+        const res = await axios.delete(`${API}/${id}`, {
+                withCredentials: true,      
+            });
+        return res.data;
+    }
+);
+
+
+
+const productSlice =createSlice({
+    name: "product",
+    initialState: {
+        list: [],
+        filtered: [],
+        newProducts: [],
+        single: null,
+        loading: false,
+        error: null,
+        successMessage: null
+    },
+    reducers: {
+        clearMessage(state){
+            state.error = null;
+            state.successMessage = null;
+        },
+        clearSingle(state) {
+            state.single = null;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            // Get All products
+            .addCase(fetchProducts.pending, (state) => { state.loading = true })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.loading = false;
+                state.list = action.payload;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+
+            // Get single product
+            .addCase(fetchProductById.fulfilled, (state, action) => {
+                state.single = action.payload;
+            })
+            .addCase(fetchProductById.pending, (state) => { state.loading = true })
+            .addCase(fetchProductById.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+
+            // Get new products
+            .addCase(fetchNewProducts.fulfilled,(state, action) => {
+                state.newProducts = action.payload;
+            })
+            .addCase(fetchNewProducts.pending, (state) => { state.loading = true })
+            .addCase(fetchNewProducts.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+            // Get filtered products
+            .addCase(fetchProductList.fulfilled, (state, action) => {
+                state.filtered = action.payload;
+            })
+            .addCase(fetchProductList.pending, (state) => { state.loading = true })
+            .addCase(fetchProductList.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+            // create product
+            .addCase(createProduct.fulfilled,(state,action)=>{
+                state.successMessage = "Product created successfully";
+                state.list.push(action.payload);
+            })
+            .addCase(createProduct.pending, (state) => { state.loading = true })
+            .addCase(createProduct.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+
+            // update product
+            .addCase(updateProduct.fulfilled,(state,action)=>{
+                state.successMessage = "Product updated successfully";
+                state.list = state.list.map(p=>p._id === action.payload._id? action.payload : p);
+                state.single = action.payload;
+            })
+            .addCase(updateProduct.pending, (state) => { state.loading = true })
+            .addCase(updateProduct.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+
+
+            // delete product
+            .addCase(deleteProduct.fulfilled,(state,action)=>{
+                state.successMessage = "Product deleted successfully";
+                state.list = state.list.filter(p=>p._id !== action.payload._id);
+            })
+            .addCase(deleteProduct.pending, (state) => { state.loading = true })
+            .addCase(deleteProduct.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+    }
+});
+
+
+export default productSlice.reducer;
