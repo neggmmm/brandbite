281c4f9fb602b448415f8be30e79712002b5c644
neggmmm <kekkdead@gmail.com>
2025-12-01 08:47:56 +0200

    resolve confilcts

--- server/src/modules/order.module/order.service.js
+++ server/src/modules/order.module/order.service.js
@@ -1,8 +1,5 @@
 import orderRepo from "./order.repository.js";
 import { calculateOrderTotals, formatCartItemsForOrder } from "./orderUtils.js";
-import { getProductById } from "../product/product.repository.js";
-import * as rewardService from "../rewards/reward.service.js";
-import * as couponService from "../coupon/coupon.service.js";
 
 class OrderService {
   // ==============================
