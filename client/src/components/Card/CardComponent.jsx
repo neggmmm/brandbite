dd6cc62a8b981cbaff2a59fd169b9c507ee74a5a
neggmmm <kekkdead@gmail.com>
2025-12-03 06:08:15 +0200

    fix card component / layout

--- client/src/components/Card/CardComponent.jsx
+++ client/src/components/Card/CardComponent.jsx
@@ -13,15 +13,15 @@ export default function CardComponent({ item, product, isReward, disabled, onCli
             {isReward ? (
                 <div className="w-1/2 lg:w-full lg:h-1/2 flex items-center gap-4">
                     {product?.imgURL ? (
-                    <img
-                        src={product.imgURL}
-                        alt={product.name}
-                        className="rounded-xl object-cover shadow-sm"
-                    />
+                        <img
+                            src={product.imgURL}
+                            alt={product.name}
+                            className="rounded-xl object-cover shadow-sm"
+                        />
                     ) : (
-                    <div className="object-cover bg-gray-100 rounded-xl flex items-center justify-center shadow-inner">
-                        <Gift className="text-gray-400" />
-                    </div>
+                        <div className="object-cover bg-gray-100 rounded-xl flex items-center justify-center shadow-inner">
+                            <Gift className="text-gray-400" />
+                        </div>
                     )}
                 </div>
             ) : (
@@ -36,47 +36,63 @@ export default function CardComponent({ item, product, isReward, disabled, onCli
 
 
             {/* RIGHT SIDE */}
-            <div className="flex lg:flex-col justify-between w-1/2 lg:w-full lg:relative">
 
-                <h3 className="flex flex-col mx-2 justify-center text-md font-semibold text-secondary">
-                    {isReward ? <span
-                        className={`text-sm font-semibold flex-1 ${isReward && canRedeem(item.pointsRequired)
-                            ? "text-on-surface"
-                            : "text-muted"
-                            }`}
-                    >
-                        {product?.name || "Reward"}
-                        {item.pointsRequired}
-                    </span> :
-                        <div className='flex lg:flex-col justify-between w-1/2 lg:w-full lg:relative'>
-                            <div
-                                className={`text-sm font-semibold flex-1 `}
-                            >
-                                
-                                {product?.name || "Product"}
-                            
-                            </div>
-                            <span className='text-xs text-[#888]'>
-                                {product.desc}
+
+
+            {isReward ?
+                <div className="flex lg:flex-col justify-between w-1/2 lg:w-full lg:relative">
+                    <h3 className="flex flex-col mx-2 justify-center text-md font-semibold text-secondary">
+                        <span
+                            className={`text-sm font-semibold flex-1 ${isReward && canRedeem(item.pointsRequired)
+                                ? "text-on-surface"
+                                : "text-muted"
+                                }`}
+                        >
+                            {product?.name || "Reward"}
                             </span>
-                            {product.basePrice} EGP
+                            {item.pointsRequired}
+                        
+                    </h3>
+                    <button
+                        disabled={disabled}
+                        className={` px-4 lg:py-2 lg:absolute lg:right-0 lg:bottom-0 rounded-tl-xl  ${!isReward ? "bg-secondary text-white" : canRedeem(item.pointsRequired) ? "bg-secondary text-white" : "bg-gray-200 text-gray-400 cursor-not-allowed"}`}
+                    >
+                        <span className="text-2xl inline-block transform transition-transform duration-500 group-hover:rotate-225">
+                            +
+                        </span>
+                    </button>
+
+
+                </div> :
+                <div className='flex lg:flex-col justify-between w-1/2 lg:w-full lg:relative'>
+                    <h3 className="flex flex-col mx-2 justify-center text-md font-semibold text-secondary">
+                        <div
+                            className={`text-sm font-semibold flex-1 `}
+                        >
+
+                            {product?.name || "Product"}
+
                         </div>
-                    }
+                        <span className='text-xs text-[#888]'>
+                            {product.desc}
+                        </span>
+                        {product.basePrice} EGP
+                    </h3>
+                    <button
+                        disabled={disabled}
+                        className={` px-4 lg:py-2 lg:absolute lg:right-0 lg:bottom-0 rounded-tl-xl  ${!isReward ? "bg-secondary text-white" : canRedeem(item.pointsRequired) ? "bg-secondary text-white" : "bg-gray-200 text-gray-400 cursor-not-allowed"}`}
+                    >
+                        <span className="text-2xl inline-block transform transition-transform duration-500 group-hover:rotate-225">
+                            +
+                        </span>
+                    </button>
+
+                </div>
+            }
+
+            {/* FULL HEIGHT BUTTON */}
 
 
-
-
-                </h3>
-                {/* FULL HEIGHT BUTTON */}
-                <button
-                    disabled={disabled}
-                    className={` px-4 lg:py-2 lg:absolute lg:right-0 lg:bottom-0 rounded-tl-xl  ${!isReward ? "bg-secondary text-white" : canRedeem(item.pointsRequired) ? "bg-secondary text-white" : "bg-gray-200 text-gray-400 cursor-not-allowed"}`}
-                >
-                    <span className="text-2xl inline-block transform transition-transform duration-500 group-hover:rotate-225">
-                        +
-                    </span>
-                </button>
-            </div>
         </div>
     )
 }
