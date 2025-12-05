a36c9dabe03a9ba3f98a98bbe2029a217454c6d5
Nour <kkid607@gmail.com>
2025-12-05 06:24:07 +0200

    !!

--- client/src/pages/admin/Users.jsx
+++ client/src/pages/admin/Users.jsx
@@ -235,11 +235,3 @@ export default function Users() {
     </div>
   );
 }
-
-export default function Users() {
-    return (
-        <div>
-            <h1 className="dark:text-white text-center  text-6xl">Users</h1>
-        </div>
-    );
-}
