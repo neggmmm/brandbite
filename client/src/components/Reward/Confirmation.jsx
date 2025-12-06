43a4f7dc3826ffed61799ad2c89e92388bf62026
neggmmm <kekkdead@gmail.com>
2025-12-06 15:48:39 +0200

    localization for reward

--- client/src/components/Reward/Confirmation.jsx
+++ client/src/components/Reward/Confirmation.jsx
@@ -1,13 +1,14 @@
+import { t } from 'i18next'
 import React from 'react'
 
 export default function Confirmation({selectedReward, onClick,onReject}) {
   return (
      <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-2xl shadow-xl w-[90%] max-w-sm text-center animate-fadeIn">
-            <h2 className="text-xl font-semibold mb-4">Confirm Redemption</h2>
+            <h2 className="text-xl font-semibold mb-4">{t("Confirm")}</h2>
             <p className="text-gray-600 mb-6">
-              Redeem <span className="font-bold">{selectedReward.title}</span> for{" "}
-              <span className="font-bold">{selectedReward.pointsRequired}</span> points?
+              {t("Redeem")} <span className="font-bold">{selectedReward.title}</span> {t("for")}{" "}
+              <span className="font-bold">{selectedReward.pointsRequired}</span> {t("points")}?
             </p>
 
             <div className="flex gap-4 justify-center">
@@ -17,14 +18,14 @@ export default function Confirmation({selectedReward, onClick,onReject}) {
                 }}
                 className="px-4 py-2 bg-secondary text-white rounded-xl"
               >
-                Yes, Redeem
+                {t("Yes")}
               </button>
 
               <button
                 onClick={onReject}
                 className="px-4 py-2 bg-gray-300 text-gray-700 rounded-xl"
               >
-                No
+                  {t("No")}
               </button>
             </div>
           </div>
--- client/src/components/Reward/MileStones.jsx
+++ client/src/components/Reward/MileStones.jsx
@@ -1,20 +1,21 @@
+import { t } from 'i18next';
 import React from 'react'
 import { FaStarOfLife } from 'react-icons/fa';
 
 export default function MileStones({milestones, points, userName, userPoints, handleViewRedemptions,progress,maxMilestone}) {
   return (
      <div className="fixed z-10  top-0 w-full md:w-9/10 dark:bg-primary/30 bg-white py-8 px-6 rounded-b-3xl shadow-lg">
-            <h1 className="text-3xl font-bold mb-4">Rewards</h1>
+            <h1 className="text-3xl font-bold mb-4"> {t('Rewards')}</h1>
     
             <div className="bg-white/20 dark:bg-primary/50 p-4 rounded-2xl backdrop-blur-md">
     
               <div className="flex justify-between items-center mb-2">
-                <span className="text-lg font-semibold">Hello, {userName}</span>
+                <span className="text-lg font-semibold">{t("Hello")}, {userName}</span>
                 <button
                   onClick={handleViewRedemptions}
-                  className="text-xs px-3 py-1 bg-secondary/80 hover:bg-secondary text-white rounded-lg transition-colors"
+                  className="text-xs px-5 py-2 bg-secondary/80 hover:bg-secondary text-white rounded-lg transition-colors"
                 >
-                  My Redemptions
+                 {t('view_redemptions')}
                 </button>
               </div>
     
