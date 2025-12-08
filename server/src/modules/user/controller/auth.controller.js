4b129fcca17b986825d4acce595bf9fa9ed1bde7
neggmmm <kekkdead@gmail.com>
2025-12-08 09:05:48 +0200

    working ?

--- server/src/modules/user/controller/auth.controller.js
+++ server/src/modules/user/controller/auth.controller.js
@@ -162,8 +162,8 @@ export const logoutController = async (req, res) => {
   try {
     const refreshToken = req.cookies.refreshToken;
     await logoutUserService(refreshToken);
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken",cookieOptions);
+    res.clearCookie("refreshToken",cookieOptions);
     return res.status(200).json({ message: "Logged out successfully" });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
