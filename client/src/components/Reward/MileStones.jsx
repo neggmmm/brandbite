3e0873b16c8e9616ff62f031c47ba52f5d9a56f8
neggmmm <kekkdead@gmail.com>
2025-12-20 19:20:38 +0200

    reward & reward order darkmode and card

--- client/src/components/Reward/MileStones.jsx
+++ client/src/components/Reward/MileStones.jsx
@@ -4,21 +4,21 @@ import ProgressBar from './ProgressBar';
 export default function MileStones({userName, userPoints, handleViewRedemptions}) {
   return (
      <div className="fixed z-10 top-0 w-full md:w-9/10 dark:bg-gray-800 bg-white py-8 px-6 rounded-b-3xl shadow-lg">
-            <h1 className="text-3xl font-bold mb-4"> {t('Rewards')}</h1>
+            <h1 className="text-3xl font-bold"> {t('Rewards')}</h1>
 
-            <div className="bg-white/20 dark:bg-gradient-to-r dark:from-secondary dark:to-secondary/70 p-4 rounded-2xl backdrop-blur-md">
+            <div className="bg-gradient-to-br dark:from-secondary/60 dark:to-secondary/40 dark:bg-black/20 px-6 py-10 rounded-2xl backdrop-blur-md">
               
               <div className="flex justify-between items-center mb-2">
-                <span className="text-lg font-semibold">{t("Hello")}, <span className='text-secondary dark:text-white'>{userName}</span></span>
+                <span className="text-lg font-semibold">{t("Hello")}, <span className='text-secondary'>{userName}</span></span>
                 <button
                   onClick={handleViewRedemptions}
-                  className="text-xs px-5 py-2 bg-secondary/80  hover:bg-secondary text-white rounded-lg transition-colors"
+                  className="text-xs px-5 py-2 hover:scale-105 transition-all bg-secondary/80  hover:bg-secondary text-white rounded-lg "
                 >
                  {t('view_redemptions')}
                 </button>
               </div>
 
-              <p className="text-4xl font-bold text-secondary dark:text-white">{userPoints}</p>
+              <p className="text-4xl font-bold text-secondary">{userPoints}</p>
 
               {/* MILESTONE PROGRESS BAR */}
               <ProgressBar Reward/>
