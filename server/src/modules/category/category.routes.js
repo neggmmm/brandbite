04135f4123e6ddf3d2dc5d4e668d984d95187208
neggmmm <kekkdead@gmail.com>
2026-02-17 23:44:00 +0200

    requireUser middle and adjust queries for categories

--- server/src/modules/category/category.routes.js
+++ server/src/modules/category/category.routes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 import { getAllCategories ,getCategoryById,addCategory,updateCategory,deleteCategory} from "./category.controller.js";
 
 import { uploadCloud } from "../../middlewares/uploadCloudinary.middleware.js";
+import authMiddleware from "../../middlewares/auth.middleware.js";
+import requireRestaurantUser from "../../middlewares/requireRestaurantUser.js";
+import roleMiddleware from "../../middlewares/role.middleware.js";
 
 router.get("/", getAllCategories);
 router.get('/:id',getCategoryById);
-router.post('/',uploadCloud.single('categoryImage'),addCategory);
-router.put('/:id',uploadCloud.single('categoryImage'),updateCategory);
-router.delete('/:id',deleteCategory)
+router.post('/', authMiddleware, requireRestaurantUser, roleMiddleware('admin'), uploadCloud.single('categoryImage'), addCategory);
+router.put('/:id', authMiddleware, requireRestaurantUser, roleMiddleware('admin'), uploadCloud.single('categoryImage'), updateCategory);
+router.delete('/:id', authMiddleware, requireRestaurantUser, roleMiddleware('admin'), deleteCategory)
 export default router;
--- server/src/modules/category/category.service.js
+++ server/src/modules/category/category.service.js
@@ -1,21 +1,21 @@
 import {getAllCategoriesRepo,getCategoryByIdRepo,addCategoryRepo,updateCategoryRepo,deleteCategoryRepo} from "./category.repository.js";
 
-export const getAllCategoriesService = async () => {
-    return await getAllCategoriesRepo();
+export const getAllCategoriesService = async (restaurantId = null) => {
+    return await getAllCategoriesRepo(restaurantId);
 }
 
-export const getCategoryByIdService = async (id) => {
-    return await getCategoryByIdRepo(id);
+export const getCategoryByIdService = async (id, restaurantId = null) => {
+    return await getCategoryByIdRepo(id, restaurantId);
 }
 
 export const addCategoryService = async (data) => {
     return await addCategoryRepo(data);
 }
 
-export const updateCategoryService = async (id,data) => {
-    return await updateCategoryRepo(id,data);
+export const updateCategoryService = async (id,data, restaurantId = null) => {
+    return await updateCategoryRepo(id,data, restaurantId);
 }
 
-export const deleteCategoryService = async (id) => {
-    return await deleteCategoryRepo(id);
+export const deleteCategoryService = async (id, restaurantId = null) => {
+    return await deleteCategoryRepo(id, restaurantId);
 }
